e32d76d180802e548885483bed70aed9a9e1ea6b
wateenalsayari <wateenwinter.tws@gmail.com>
2024-02-29 23:24:08 +0000

    importing text file + implemented find_neighbours

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+#creating a list of the words from words_alpha.txt
+f = open('words_alpha.txt')
+words_list = []
+for word in f.read().split():
+    words_list.append(word)
 class Game:
-    def find_neighbours:
-        pass
+
+    def find_neighbours(self, target_word, lst):
+        neighbours = []
+        words = []
+        count = 0
+        target_letters = [*target_word]
+        for w in lst:
+            if len(w) == len(target_word):
+                words.append(w)
+        for w in words:
+            letters = [*w]
+            for i in range(len(w)):
+                if letters[i] == target_letters[i]:
+                    count += 1
+            if count == (len(target_word) - 1):
+                neighbours.append(w)
+            count = 0
+        return neighbours
 
     def input_start_word(self, start_word):
         return start_word
