705a32a03d5203069d7f2cfcc88f854c40c7f6af
Roman <127163323+zodackwand@users.noreply.github.com>
2024-02-29 13:35:10 +0000

    Added input functions

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 class Game:
     def find_neighbours:
         pass
-    
+
+    def input_start_word(self, start_word):
+        return start_word
+
+    def input_end_word(self, end_word):
+        return end_word
