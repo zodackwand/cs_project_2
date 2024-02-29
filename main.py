57042c5300e2e528655293f752aa369f47ecaf5b
Roman <127163323+zodackwand@users.noreply.github.com>
2024-02-29 13:48:11 +0000

    Added printing results func

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+import time
+
 class Game:
-    def find_neighbours:
+    def find_neighbours(self):
         pass
 
-    def input_start_word(self, start_word):
-        return start_word
+    def word_ladder(self):
+        pass
 
-    def input_end_word(self, end_word):
-        return end_word
+def main():
+    # Initialize the class and ask input
+    game = Game()
+    start_word = input()
+    end_word = input()
+
+    # Get the path
+    start_time = time.time()
+    path_array = game.word_ladder(start_word, end_word)
+    end_time = time.time()
+
+    execution_time = end_time - start_time
+
+    # Print the results
+    for i in path_array:
+        print(i, end='')
+
+    # Print execution time
+    print(execution_time)
